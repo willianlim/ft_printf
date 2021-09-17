5fed77325ccdbc88f6b5f50106a75c9bd41da79e
Willian-code <willianrosendodelima@outlook.com>
2021-09-17 12:37:46 -0300

    change

--- source/screen_u.c
+++ source/screen_u.c
@@ -6,13 +6,13 @@
 /*   By: wrosendo <wrosendo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 17:38:16 by wrosendo          #+#    #+#             */
-/*   Updated: 2021/09/09 17:42:04 by wrosendo         ###   ########.fr       */
+/*   Updated: 2021/09/17 12:35:41 by wrosendo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static int	count(long int n)
+static int	count(long long int n)
 {
 	int	len;
 
@@ -28,15 +28,15 @@ static int	count(long int n)
 	return (len);
 }
 
-char	*u_itoa(long int n)
+char	*u_itoa(long long int n)
 {
-	long int	len;
+	long long int	len;
 	int			neg;
 	char		*tmp;
 
 	neg = 0;
-	if (n == 4294967294)
-		return (ft_strdup("4294967294"));
+	if (n > 4294967295)
+		return (ft_strdup(u_itoa(n - 4294967296)));
 	len = count(n);
 	tmp = malloc(sizeof(char) * (len + 1));
 	if (tmp == NULL)
@@ -66,9 +66,9 @@ static void	disable_flags(t_option *opt)
 int	screen_u(t_option *opt)
 {
 	char		*s;
-	long int	n;
+	long long int	n;
 
-	n = va_arg((*opt).ap, long int);
+	n = va_arg((*opt).ap, long long int);
 	s = u_itoa(n);
 	(*opt).number = s;
 	disable_flags(opt);
